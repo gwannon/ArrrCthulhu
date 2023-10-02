f504e8121b41e82ef4bc594ebf43438771c5b02b
Gwannon <monclus.jorge@gmail.com>
2023-10-02 09:54:53 +0200

    Minor fixes

--- generatePDF.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-service mysql stop
-chromium --headless --no-sandbox --no-pdf-header-footer --print-to-pdf=/root/ArrrCthulhu.pdf https://arrrcthulhu.com/
-exiftool -overwrite_original -Title="¡Arrr! Cthulhu - Aventuras de piratas en el Caribe de los mitos de Cthulhu." -Author="@Gwannon" -Subject="¡Arrr! Cthulhu es una ambientación para SWADE con el que podrás jugar aventuras de piratas en el Caribe de los Mitos de Cthulhu. Versión 1.0.1" /root/ArrrCthulhu.pdf
-cp /root/ArrrCthulhu.pdf /var/www/vhosts/arrrcthulhu.gwannon.com/ArrrCthulhu/ArrrCthulhu.pdf
-service mysql start
