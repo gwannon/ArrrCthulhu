764e0bba3fc2528aa4346e72a67878407eacd979
gwannon <monclus.jorge@gmail.com>
2025-03-11 15:03:35 +0100

    Mejoras diseño

--- pdf.sh
+++ pdf.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-mv index.html temp.html
+mv ./index.html ./temp.html
 php ./tools/generateDocs.php
 chromium --no-sandbox --headless --gpu --no-pdf-header-footer --print-to-pdf=./temp.pdf ./index.html
 pdftk 'temp.pdf' update_info_utf8 'metas.txt' output 'ArrrCthulhu.pdf'
 rm metas.txt
 rm temp.pdf
-mv temp.html index.html
+mv -f ./temp.html ./index.html
