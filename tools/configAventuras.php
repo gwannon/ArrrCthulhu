23fb3ac39137ab6aa655c6e3fc5b623e1afe6c5b
Gwannon <monclus.jorge@gmail.com>
2026-03-08 23:46:56 +0100

    Aventuras 0.3.2

--- tools/configAventuras.php
+++ tools/configAventuras.php
@@ -5,12 +5,12 @@ $tags = [
   'HASH' => date("YmdH"),
   'TITLE' => "Contra la Espiga Púrpura",
   'SHORTDESCRIPTION' => 'Aventura para ¡Arrr! Cthulhu',
-  'DESCRIPTION' => "«Contra la Espiga Púrpura» es una campaña para ¡Arrr! Cthulhu donde te enfrentarás a Waylunor y su culto de adoradores.",
+  'DESCRIPTION' => "Una campaña para ¡Arrr! Cthulhu donde te enfrentarás al dios exterior Waylunor y su culto de adoradores.",
   'VERSION' => "0.3.2",
   'LANZAMIENTO' => 'Mayo 2026',
   "AUTHOR" => "Jorge Monclús",
   "AUTHOR_URL" => 'https://gwannon.com',
   "KEYWORDS" => "juego de rol, Savage Worlds, cthulhu, piratas",
-  "URL" => "https://arrrcthulhu.com",
+  "URL" => "https://arrrcthulhu.com/indexAventuras.html",
   "GITHUB" => 'https://github.com/gwannon/ArrrCthulhu'
 ];
